add2e7b2dc28036f11e4274b08e0c3023f285db1
miklosbalazsi@gmail.com <miklosbalazsi@gmail.com>
2020-03-15 17:59:33 +0100

    print all info

--- E18/py_tools/list_devices.py
+++ E18/py_tools/list_devices.py
@@ -6,5 +6,15 @@ import serial.tools.list_ports
 serialPortList = serial.tools.list_ports.comports()
 
 for serialPort in serialPortList:
-    print("Name : " + serialPort.name)
+    print("device : " + serialPort.device)
+    print("name : " + serialPort.name)
+    print("description : " + serialPort.description)
     print("hwid : " + serialPort.hwid)
+    print("vid : " + serialPort.vid)
+    print("pid : " + serialPort.pid)
+    print("serial_number : " + serialPort.serial_number)
+    print("location : " + serialPort.location)
+    print("manufacturer : " + serialPort.manufacturer)
+    print("product : " + serialPort.product)
+    print("interface : " + serialPort.interface)
+    print("\n")
