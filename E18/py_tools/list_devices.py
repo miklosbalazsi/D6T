dbfb1479383afa490131043154c94a34348a2426
miklosbalazsi@gmail.com <miklosbalazsi@gmail.com>
2020-03-15 17:19:03 +0100

    Add py_tools

--- E18/py_tools/list_devices.py
+++ E18/py_tools/list_devices.py
@@ -3,7 +3,7 @@
 import serial
 import serial.tools.list_ports
 
-serialPortList = list_ports()
+serialPortList = serial.tools.list_ports.comports()
 
 for serialPortList in serialPort:
     print("Name : " + serialPort.name)
