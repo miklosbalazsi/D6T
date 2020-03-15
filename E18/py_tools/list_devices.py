2fc6f0ebfa49d2fb5bef89999ff6a59d48f7a8f9
miklosbalazsi@gmail.com <miklosbalazsi@gmail.com>
2020-03-15 17:16:08 +0100

    Add py_tools

---
+++ E18/py_tools/list_devices.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import serial
+import serial.tools.list_ports
+
+serialPortList = list_ports()
+
+for serialPortList in serialPort:
+    print("Name : " + serialPort.name)
+    print("hwid : " + serialPort.hwid)
