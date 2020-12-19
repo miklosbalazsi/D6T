f9f8a7dff00cde8a5e6b4fcd8aaba9efca428556
Miklos Balazsi <miklos.balazsi@sagemcom.com>
2020-12-19 14:55:24 +0100

    init UART receiving

--- E18/serial/main.h
+++ E18/serial/main.h
@@ -1,9 +1,1 @@
-#define uint unsigned int
-#define uchar unsigned char
-#define bin(a,b,c,d,e,f,g,h) (a<<7)|(b<<6)|(c<<5)|(d<<4)|(e<<3)|(f<<2)|(g<<1)|(h<<0)
-#define setBit(reg, bit) reg |= (1 << bit)
-#define clearBit(reg, bit) reg &= ~(1 << bit)
-
-void delayms(uint xms);
-void initUart0();
 void init32MhzClock();
