bb1193a17094154daf58551fb07bc4660a7ea84c
Miklos Balazsi <miklosbalazsi@gmail.com>
2021-05-07 15:33:16 +0100

    xbee

---
+++ xbee/python/xbee.py
@@ -0,0 +1,16 @@
+from digi.xbee.devices import XBeeDevice
+device = XBeeDevice("/dev/ttyUSB0", 9600)
+device.open()
+
+print(device.read_device_info())
+
+protocol = device.get_protocol()
+print(protocol)
+
+
+print("get_64bit_addr "+device.get_64bit_addr())
+print("get_16bit_addr "+device.get_16bit_addr())
+
+
+
+device.close()
